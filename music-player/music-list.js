57ef52bd370394fbcd74ec13f8588f3cef071506
neschii <mannpisn@gmail.com>
2024-11-04 21:21:14 -0300

    photos + songs

--- music-player/music-list.js
+++ music-player/music-list.js
@@ -2,27 +2,59 @@ let allMusic = [
     { 
         music: "Guilty as Sin?",
         artist: "Taylor Swift",
-        img: "music-1",
-        src: "music1"
+        img: "taylor",
+        src: "guilty",
+        time: "04:14"
     },
 
     { 
         music: "BITTERSUITE",
         artist: "Billie Eilish",
-        img: "music-2",
-        src: "music-2"
+        img: "billie",
+        src: "bittersuite",
+        time: "04:58"
     },
 
     { 
         music: "bet u wanna",
         artist: "Sabrina Carpenter",
-        img: "music-3",
-        src: "music-3"
+        img: "sabrina",
+        src: "betuwanna",
+        time: "03:11"
     },
         { 
         music: "Boyfriend",
         artist: "Dove Cameron",
-        img: "music-4",
-        src: "music-4"
+        img: "dovecameron",
+        src: "boyfriend",
+        time: "02:33"
+    },
+        { 
+        music: "older",
+        artist: "Isabel LaRosa",
+        img: "Isabel",
+        src: "older",
+        time: "02:17"
+    },
+            { 
+        music: "BABYDOLL",
+        artist: "Ari Abdul",
+        img: "Ari",
+        src: "BABYDOLL",
+        time: "03:16"
+    },
+            { 
+        music: "Daddy Issues",
+        artist: "The Neighbourhood",
+        img: "neighbourhood",
+        src: "daddy",
+        time: "04:20"
+    },
+            { 
+        music: "Naked",
+        artist: "Avril Lavigne",
+        img: "music-5",
+        src: "naked",
+        time: "03:28"
     },
 ]
--- music-player/script.js
+++ music-player/script.js
@@ -2,11 +2,15 @@ wrapper = document.querySelector(".wrapper"),
 musicImage = wrapper.querySelector(".img-card img"),
 musicArtist = wrapper.querySelector(".song-name .artist"),
 musicName = wrapper.querySelector(".song-name .music"),
+musicTime = wrapper.querySelector(".last-time"),
+musicDuration = wrapper.querySelector(".total-time")
 mainAudio = wrapper.querySelector("#main-audio"),
-nextBtn = wrapper.querySelector("#next-btn");
+nextBtn = wrapper.querySelector("#next-btn"),
+playPause = wrapper.querySelector("#play"),
+trackBar = wrapper.querySelector("#progressBar");
+
 
 let musicIndex = 1;
-
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
 });
@@ -14,6 +18,7 @@ window.addEventListener("load", () => {
 function loadMusic(indexNumb) {
     musicName.innerText = allMusic[indexNumb - 1].music;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicTime.innerText = allMusic[indexNumb -1].time;
     musicImage.src = `img/${allMusic[indexNumb - 1].img}.jpg`;
     mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
 }
@@ -22,12 +27,33 @@ function nextMusic() {
     musicIndex++;
     musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
      loadMusic(musicIndex);
+    playMusic(mainAudio.play);
 }
-
 
 function previousMusic() {
     musicIndex--;
     musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
      loadMusic(musicIndex);
+         playMusic(mainAudio.play);
 }
+        function playMusic() {
+         if (mainAudio.paused) { 
+             mainAudio.play();
+        } else { 
+            mainAudio.pause();
+        }
+     }
+
+mainAudio.addEventListener("timeupdate", (e)=> {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    let progressWidth = (currentTime / duration ) * 100;
+    trackBar.style.width = `${progressWidth}%`
+
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+        if(currentSec < 10) { currentSec = `0${currentSec}`; } 
+    musicDuration.innerText = `${currentMin}:${currentSec}`
+
+})
 
